2a2255ec9981fee86ad0c66b929e46b7c60d91ad
Han Lynn <Han Lynn>
2016-08-18 23:57:21 -0700

    send ajax

--- js/script.js
+++ js/script.js
@@ -4,12 +4,8 @@ let email="", type=""
 
 $('#email-button').click(()=>{
     email = $('#email').val()
-    console.warn(email);
     if(validateEmail(email)) {
-        $('#sub-content-1').fadeOut( "", function() {
-            $('#sub-content-1').addClass("hidden")
-            $('#sub-content-2').fadeIn().removeClass("hidden")
-        })    
+        showOptions()
     } else {
         alert("Please enter a valid email address")
     }
@@ -17,14 +13,41 @@ $('#email-button').click(()=>{
 
 $('#done-button').click(()=>{
     type = $('input[name="type"]:checked').val();
-    console.warn(type);
+    sendRequest(showFinalPage);
+})
+
+function showOptions() {
+    $('#sub-content-1').fadeOut( "", function() {
+        $('#sub-content-1').addClass("hidden")
+        $('#sub-content-2').fadeIn().removeClass("hidden")
+    })    
+}
+
+function showFinalPage() {
     $('#content-1').fadeOut( "", function() {
         $('#content-1').addClass("hidden")
         $('#content-2').fadeIn().removeClass("hidden")
     })
-})
+}
 
 function validateEmail(email) {
     var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
+}
+
+function sendRequest(successCallback){
+    const apiKey = "$2y$10$kvyRe67FHwglsIhoz4oype0nsC0GKSwTycBrVniR49bSCthrCBgby"
+    const url = "http://platform.bantu.life/api/v1/addEmail?apikey=" + apiKey
+    var request = $.post( url, function() {
+        successCallback()
+    })
+    .done(function() {
+        
+    })
+    .fail(function() {
+        
+    })
+    .always(function() {
+
+    });
 }
